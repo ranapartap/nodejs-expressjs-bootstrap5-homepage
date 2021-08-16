9b0f09b31eca2b1650312ba3caf400bb79b744ea
rana partap <rana.partap.india@gmail.com>
2021-08-16 13:11:55 +0530

    nodejs expressjs bootstrap5 html page

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+app.listen(8000)
+
+const pathStatic = path.join(__dirname, '../public');
+const pathTemplates = path.join(__dirname, '../templates/views');
+const pathPartials = path.join(__dirname, '../templates/partials');
+
+
+/** TEMPLATE ENGINE : HBS
+ ******************************************************/
+app.use(express.static(pathStatic));
+app.set('views', pathTemplates);
+
+hbs.registerPartials(pathPartials);
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    userName : 'RANA'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('*', (req, res) => {
+  res.render('404')
+})
+
+
+/** STATIC SITE
+******************************************************/
+/*
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.get('/about', function (req, res) {
+  res.send('Hello World About page')
+})
+
+ */
